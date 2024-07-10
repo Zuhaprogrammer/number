3063e1a9bd349fcdcff95c2430d46e27c78f057a
Khurshid Eshonkulov <khurshideshonkulov@Khurshids-MacBook-Pro.local>
2024-07-10 21:29:40 +0500

    done perfect numbers

--- op_numbers/src/Main.java
+++ op_numbers/src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("enter number: ");
+        int n = scanner.nextInt();
+        printPerfectNumbers(n);
+    }
+
+    public static void printPerfectNumbers(int n) {
+        for (int num = 1; num <= n; num++) {
+            if (isPerfect(num)) {
+                System.out.println(num);
+            }
+        }
+    }
+
+    public static boolean isPerfect(int num) {
+        int summa = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                summa += i;
+            }
+        }
+        return summa == num;
     }
 }
