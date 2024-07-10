0eab1c0ddcc709e69e0e387ee2d563a06d7ebc0f
Jamoliddin Saydirasulov <154518209+Jamoliddin007@users.noreply.github.com>
2024-07-10 21:13:21 +0500

    even numbers method is added

--- op_numbers/src/Main.java
+++ op_numbers/src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int num = scanner.nextInt();
+        even_numbers(num);
+    }
 
+    static void even_numbers(int n) {
+        if (n % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
     }
 }
