b2a252c44a38207c24096ee0cbf8885eb9901e06
Madamin45 <madaminqahramonov45@gmail.com>
2024-07-10 21:09:15 +0500

    this metod completed

--- op_numbers/src/Main.java
+++ op_numbers/src/Main.java
@@ -1,7 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a--> ");
+        int a = scanner.nextInt();
+        System.out.print("Enter b--> ");
+        int b = scanner.nextInt();
+        ekuk(a, b);
+    }
+    public static void ekuk(int a, int b){
+        int ekuki = a;
+        while(true){
+            if (ekuki%a == 0 && ekuki%b == 0){
+                System.out.println("Ekuk " + a + " and " + b + " is: " + ekuki);
+                return;
+            }
+            ekuki++;
+        }
     }
 }
