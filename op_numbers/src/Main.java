2503ad9714baec398a3b1c111117cb1665228a43
Zuhaprogrammer <azizprogrammer05@gmail.com>
2024-07-10 22:17:33 +0500

    done prime numbers

--- op_numbers/src/Main.java
+++ op_numbers/src/Main.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter n: ");
+        int n = scanner.nextInt();
+        primeNumbers(n);
+    }
+
+    private static void primeNumbers(int n) {
+        int count;
+        for (int i = 1; i < n; i++) {
+            count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (i == 1) {
+                System.out.print(i + "\t");
+            }
+            if (count == 2) {
+                System.out.print(i + "\t");
+            }
+        }
     }
 }
